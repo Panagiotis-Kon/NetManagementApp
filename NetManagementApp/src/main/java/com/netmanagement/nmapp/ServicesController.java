1865a2c539712d1b4c566e6d5a87aa0bdbc1a68a
alextsak <alextsak93@gmail.com>
2015-07-20 16:18:43 +0300

    com.customer.info package is out

--- NetManagementApp/src/main/java/com/netmanagement/nmapp/ServicesController.java
+++ NetManagementApp/src/main/java/com/netmanagement/nmapp/ServicesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.customer.info.Customer;
+
 import com.google.gson.Gson;
 import com.netmanagement.csvdatasets.ParseCSV;
 import com.netmanagement.dataprocessing.AccessPointCalculations;
