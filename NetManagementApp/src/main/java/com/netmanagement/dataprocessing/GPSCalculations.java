24580bbe3a2988654797e6a54f8857509189ee14
Emanon <emanon.sonex@gmail.com>
2015-07-22 16:41:43 +0300

    22/07/2015 4:41 panos

--- NetManagementApp/src/main/java/com/netmanagement/dataprocessing/GPSCalculations.java
+++ NetManagementApp/src/main/java/com/netmanagement/dataprocessing/GPSCalculations.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
 import com.netmanagement.csvdatasets.ParseGPS;
 import com.netmanagement.entities.GPS;
 
@@ -26,7 +27,7 @@ public class GPSCalculations {
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public ArrayList<GPS> searchUser(String userID, String startDate, String endDate){
-		
+		//For given variables find gps points
 		System.out.println("userID: " + userID + " startDate: " + startDate + " endDate: " + endDate);
 		HashMap<String, ArrayList<GPS>> hap = ParseGPS.getInstance().getHap();
 		ArrayList<GPS> alist = new ArrayList<GPS>();
@@ -65,6 +66,134 @@ public class GPSCalculations {
 			System.out.println("GPSCalculations: alist is empty!!!");
 		}
 		return alist;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public ArrayList<GPS> findStayPoints(ArrayList<GPS> GPSPoints, String Tmin,
+			String Tmax, Double Dmax) { // List of specific gps points
+		// For given variables find stay points, Tmin and Tmax are in format dd:HH:mm:ss
+		System.out.println("uTmin: " + Tmin + " Tmax: " + Tmax + " Dmax: "
+				+ Dmax);
+		ArrayList<GPS> Lsp = new ArrayList<GPS>();
+        int i=1, j=0, numofStayPoints=0;
+        String t=null;
+        double d=0;
+        while (i<GPSPoints.size()){
+        	j=i+1;
+        	while (j<GPSPoints.size()){
+        		t=TimeDifference(GPSPoints.get(j).getTimestamp(),GPSPoints.get(j-1).getTimestamp());
+        		SimpleDateFormat sdf = new SimpleDateFormat("dd:HH:mm:ss");
+        		Date datet=null;
+        		Date dateTmax=null;
+        		Date dateTmin=null;
+        		try {
+					datet=sdf.parse(t);
+					dateTmax=sdf.parse(Tmax);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        		if (datet.after(dateTmax)){
+        			i=j;
+        			break;
+        		}
+        		d=SpaceDistance(GPSPoints.get(i),GPSPoints.get(j));
+        		if (d>Dmax){
+        			t=TimeDifference(GPSPoints.get(i).getTimestamp(),GPSPoints.get(j-1).getTimestamp());
+        			try {
+    					datet=sdf.parse(t);
+    					dateTmin=sdf.parse(Tmin);
+    				} catch (ParseException e) {
+    					// TODO Auto-generated catch block
+    					e.printStackTrace();
+    				}
+        			if (datet.after(dateTmin)){
+        				//lat,lon from centroid
+        				double lat=GPSPoints.get(i).getUlatitude();
+        				double lon=GPSPoints.get(i).getUlongtitude();
+        				for (int k=i+1;k<j-1;k++){
+        					lat=estimateCentroid(GPSPoints.get(k).getUlatitude(), lat);
+        					lon=estimateCentroid(GPSPoints.get(k).getUlongtitude(), lon);
+        				}
+        				//Tstart
+        				String Tstart = GPSPoints.get(i).getTimestamp();
+        				//Tend
+        				String Tend = GPSPoints.get(j-1).getTimestamp();
+        				//sp
+        				GPS sp = new GPS();
+        				numofStayPoints++;
+        				String data[] = new String[]{Integer.toString(numofStayPoints),GPSPoints.get(i).getUser(),Double.toString(lat),Double.toString(lon),Tstart+" to "+Tend};
+        				sp.setAll(data);
+        				//Lsp
+        				Lsp.add(sp);
+        			}
+        			i=j;
+        			break;
+        		}
+        		j=j+1;
+        	}
+        }
+		if (Lsp.isEmpty()) {
+			System.out.println("GPSCalculations: alist is empty!!!");
+		}
+		return Lsp;
+	}
+	
+	String TimeDifference(String string1, String string2){
+		//Find time difference between given variables and return it to dd:HH:mm:ss format
+		String TimeDiff=null;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date d1 = null;
+		Date d2 = null;
+		try {
+			d1 = format.parse(string1);
+			d2 = format.parse(string2);
+ 
+			//in milliseconds
+			long diff = d2.getTime() - d1.getTime();
+ 
+			long diffSeconds = diff / 1000 % 60;
+			long diffMinutes = diff / (60 * 1000) % 60;
+			long diffHours = diff / (60 * 60 * 1000) % 24;
+			long diffDays = diff / (24 * 60 * 60 * 1000);
+			TimeDiff=System.out.format("%02d\n", diffDays)+":"+System.out.format("%02d\n", diffHours)+":"+System.out.format("%02d\n", diffMinutes)+":"+System.out.format("%02d\n", diffSeconds);
+ 
+			System.out.print(diffDays + " days, ");
+			System.out.print(diffHours + " hours, ");
+			System.out.print(diffMinutes + " minutes, ");
+			System.out.print(diffSeconds + " seconds.");
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return TimeDiff;
+	}
+	
+	double SpaceDistance(GPS gps1, GPS gps2){
+		//Find Pythagorean distance calculation between given variables
+		double distance=0,lat=0,lon=0;
+		if (gps1.getUlatitude()>gps2.getUlatitude()){
+			lat=gps1.getUlatitude()-gps2.getUlatitude();
+		}
+		else {
+			lat=gps2.getUlatitude()-gps1.getUlatitude();
+		}
+		lat=lat*lat;
+		if (gps1.getUlongtitude()>gps2.getUlongtitude()){
+			lon=gps1.getUlongtitude()-gps2.getUlongtitude();
+		}
+		else {
+			lon=gps2.getUlongtitude()-gps1.getUlongtitude();
+		}
+		lon=lon*lon;
+		distance=Math.sqrt(lat+lon);
+		return distance;
+	}
+	
+	double estimateCentroid(double value1,double value2){
+		double center=0;
+		center=(value1+value2)/2;
+		return center;
 	}
 
 }
