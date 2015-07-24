bd48d8f0dbac9e26cbaca93198361b6229e79d7f
alextsak <alextsak93@gmail.com>
2015-07-24 16:15:30 +0300

    24/7/2015 4:15 tsak -> everything ok except battery

--- NetManagementApp/src/main/webapp/resources/js/gmaps.js
+++ NetManagementApp/src/main/webapp/resources/js/gmaps.js
@@ -3,49 +3,55 @@ var view = -1; // 0-> markers, 1->polyline, 2->cells
 
 function Markers(data) {
 
-		view = 0;
-    	//location of marker
-		//var myLatlng=null;
-		var latlng = null;
-		//var marker = null;
- 		sessionStorage.setItem('sent',JSON.stringify(data));
- 		
- 		$.each(data,function(i,item){
- 			latlng = new google.maps.LatLng(item.APlatitude, item.APlongtitude);
- 			if(i==0){
- 				
- 				
- 		      	var mapOptions = {
- 		    		zoom: 8,
- 		    		center: latlng,
- 		    		mapTypeId: google.maps.MapTypeId.TERRAIN
- 		 		 };
- 		      	
- 		 		 map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
- 	
- 			}
- 			
- 			var marker = new google.maps.Marker({
-	                position: latlng,
-	                map: map,
-	               title: item.ssid,
-	                animation: google.maps.Animation.DROP,
-	            });
-		 		
-		 		var content = "SSID: " + item.ssid + "\n" + " Mean RSSI: " + item.rssi + "\n" + '</h3>' + " frequency: " + item.frequency;     
+		if(ap == 1) {
+			
+			view = 0;
+			var latlng = null;
+			sessionStorage.setItem('sent',JSON.stringify(data));
+	 		
+	 		$.each(data,function(i,item){
+	 			latlng = new google.maps.LatLng(item.APlatitude, item.APlongtitude);
+	 			if(i==0){
+	 				
+	 				
+	 		      	var mapOptions = {
+	 		    		zoom: 8,
+	 		    		center: latlng,
+	 		    		mapTypeId: google.maps.MapTypeId.TERRAIN
+	 		 		 };
+	 		      	
+	 		 		 map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	 	
+	 			}
+	 			
+	 			var marker = new google.maps.Marker({
+		                position: latlng,
+		                map: map,
+		               title: item.ssid,
+		                animation: google.maps.Animation.DROP,
+		            });
+			 		
+			 		var content = "SSID: " + item.ssid + "\n" + " Mean RSSI: " + item.rssi + "\n" + '</h3>' + " frequency: " + item.frequency;     
 
-	 			var infowindow = new google.maps.InfoWindow();
+		 			var infowindow = new google.maps.InfoWindow();
 
-	 			google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
-	 			        return function() {
-	 			           infowindow.setContent(content);
-	 			           infowindow.open(map,marker);
-	 			        };
-	 			    })(marker,content,infowindow)); 
- 			
- 			
- 		});
-		console.log('ending scipt');
+		 			google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+		 			        return function() {
+		 			           infowindow.setContent(content);
+		 			           infowindow.open(map,marker);
+		 			        };
+		 			    })(marker,content,infowindow)); 
+	 			
+	 			
+	 		});
+			console.log('ending scipt');
+			
+			
+		} 
+		else {
+			alert('Please import wifi dataset first');
+		}
+		
                                
  
 }
@@ -54,25 +60,34 @@ function Markers(data) {
 function Polyline(){
 	if( ap == 1) {
 		
-		view = 1;
-		var sentdata = JSON.parse(sessionStorage.getItem('sent'));
-		var flightPlanCoordinates = [];
-		$.each(sentdata,function(i,item){
-			
-			flightPlanCoordinates.push(new google.maps.LatLng(item.APlatitude, item.APlongtitude));
-			
-			
-		});
-		
-		var flightPath = new google.maps.Polyline({
-			path: flightPlanCoordinates,
-			geodesic: true,
-			strokeColor: '#FF0000',
-			strokeOpacity: 1.0,
-			strokeWeight: 2
+		if(map == null) {
+			Markers(data);
+		}
+		else {
+			view = 1;
+			var sentdata = JSON.parse(sessionStorage.getItem('sent'));
+			var flightPlanCoordinates = [];
+			$.each(sentdata,function(i,item){
+				
+				flightPlanCoordinates.push(new google.maps.LatLng(item.APlatitude, item.APlongtitude));
+				
+				
 			});
-		flightPath.setMap(map);
+			
+			var flightPath = new google.maps.Polyline({
+				path: flightPlanCoordinates,
+				geodesic: true,
+				strokeColor: '#FF0000',
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+				});
+			flightPath.setMap(map);
+	
+		}
 		
+	}
+	else {
+		alert('Please import wifi dataset first');
 	}
 	
 	
@@ -92,7 +107,7 @@ function Cells() {
 	
 	if(bs == 1){
 		view = 2;
-		console.log("Trying to gather cell info")
+		//console.log("Trying to gather cell info")
 		getCellsInfo();
 		
 	}
@@ -105,10 +120,28 @@ function Cells() {
 }
 
 function DrawCells() {
-	console.log("DrawCells function");
-	var bsdata = JSON.parse(sessionStorage.getItem('bs'));
+	//console.log("DrawCells function");
 	var latlng = null;
-	//var marker = null;
+	latlng = new google.maps.LatLng('37.58','23.46');
+	var bsdata = JSON.parse(sessionStorage.getItem('bs'));
+	if(map == null) { // if a map does not exists create it
+		
+		for(var item in bsdata) {
+			latlng = new google.maps.LatLng(item.BSlatitude, item.BSlongtitude);
+			break;
+		}
+		var mapOptions = {
+		    		zoom: 8,
+		    		center: latlng,
+		    		mapTypeId: google.maps.MapTypeId.TERRAIN
+		 		 };
+		      	
+		  map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	}
+	
+	
+	
+	
 	var pinColor = "52bdb0";
 	var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
 	        new google.maps.Size(21, 40),
@@ -126,9 +159,7 @@ function DrawCells() {
             });
 			   
 			   
-			   /*   */
-			   
-			//marker.setIcon('http:// labs.google.com/ridefinder/images/mm_20_green.png');
+			 
 	 		var content = "Operator: " + item.Operator + "\n" + " MCC: " + item.mcc + "\n"  + " MNC: " + item.mnc + "\n" 
 	 		+  " cid: " + item.cid + "\n" + " LAC: " + item.lac;     
 
@@ -148,18 +179,9 @@ function DrawCells() {
 }
 
 function fullscreenMode(){
-	if(view == 0) {
-		// open full screen for markers
-		window.open('fullscreenMap', '_blank');
-	}
-	else if(view == 1) {
-		// open full screen for polyline
-		window.open('fullscreenMap', '_blank');
-	}
 	
-	else if(view == 2){
-		// open full screen for cells
-	}
+		window.open('fullscreenMap', '_blank');
+	
 }
 
 
